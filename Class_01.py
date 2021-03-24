e53a97a1ca7df9e622b3dad519b87f6b7cd01e38
Lister Ogusuku Ribeiro <listeror@al.insper.edu.br>
2021-03-24 18:31:14 -0300

    second commit

--- Class_01.py
+++ Class_01.py
@@ -1,16 +1,26 @@
 
-lista = []
+# lista = []
 
-i = 0
-c = 0
+# i = 0
+# c = 0
 
-while i < 5:
-    n = float(input(f'Digite a nota do aluno {i+1}: '))
-    c += n
-    lista.append(n)
-    i+=1
+# while i < 5:
+#     n = float(input(f'Digite a nota do aluno {i+1}: '))
+#     c += n
+#     lista.append(n)
+#     i+=1
 
-print(f'A média do aluno foi: {c/5}')
+# print(f'A média do aluno foi: {c/5}')
 
 # ------
 
+
+numbers = []
+
+i = 0
+
+while i < 5:
+    n = float(input('Digite um número a ser adicionado à lista: '))
+    numbers.append(n)
+    i+=1
+    
